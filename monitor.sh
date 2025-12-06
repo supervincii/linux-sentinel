259d207dbb24d8f8ca237854ed3e20657412f022
supervincii <74002397+supervincii@users.noreply.github.com>
2025-12-06 23:41:32 +0800

    calculate CPU load avg from /proc/loadavg using grep

--- monitor.sh
+++ monitor.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# TODO
+# Use awk have floating point values on the current computations
 memory_usage_percentage() {
     total_memory=$(grep -i 'memtotal' /proc/meminfo | grep -o '[[:digit:]]\+')
     available_memory=$(grep -i 'memavailable' /proc/meminfo | grep -o '[[:digit:]]\+')
@@ -8,4 +10,10 @@ memory_usage_percentage() {
     echo "${usage_percentage}"
 }
 
+cpu_load_avg() {
+    load_avg=$(grep -o '^[0-9.]\+' /proc/loadavg)
+    echo "${load_avg}"
+}
+
 echo "Total RAM Usage: $(memory_usage_percentage)%"
+echo "CPU Load Average: $(cpu_load_avg)"
